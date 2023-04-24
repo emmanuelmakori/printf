9d37a486cc3e2eff26f2f09215fc425a8a427b11
emmanuelmakori <makmanuel101@gmail.com>
2023-04-23 21:16:45 -0700

    print statements

---
+++ holberton.h
@@ -0,0 +1,13 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+int _putchar(char character);
+void _puts(char *string);
+int _strlen(char *string);
+int _atoi(char *string);
+
+#endif /* HOLBERTON_H */
